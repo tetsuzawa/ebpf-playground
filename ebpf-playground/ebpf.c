d44f1bf17321f0135f21f36f0ca4fbcab52578c3
tetsuzawa <tetsu.takizawa5@gmail.com>
2022-01-15 18:59:25 +0900

    Use map instead of single pipe

--- ebpf-playground/ebpf.c
+++ ebpf-playground/ebpf.c
@@ -1,6 +1,19 @@
+BPF_HASH(clones);
+
 int hello_world(void *ctx) {
     u64 uid;
+    u64 counter = 0;
+    u64 *p;
+
     uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+    p = clones.lookup(&uid);
+
+    if ( p != 0 ) {
+        counter = *p;
+    }
+    counter++;
+    clones.update(&uid, &counter);
+
     // bpf_trace_printk("Hello world\n");
     bpf_trace_printk("id: %d\\n", uid);
     return 0;
--- ebpf-playground/ebpf.py
+++ ebpf-playground/ebpf.py
@@ -1,9 +1,18 @@
 from bcc import BPF
 from time import sleep
-import bcc
 
 
 b = BPF(src_file="./ebpf.c")
 clone = b.get_syscall_fnname("clone")
 b.attach_kprobe(event=clone, fn_name="hello_world")
-b.trace_print()
+# b.trace_print()
+
+while True:
+    sleep(2)
+    s = ""
+    if len(b["clones"].items()):
+        for k, v in b["clones"].items():
+            s += f"ID {k.value}: {v.value}\t"
+        print(s)
+    else:
+        print("No entries yes")
